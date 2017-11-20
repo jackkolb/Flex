b16c377025f0695ae8696846d9d8184ef6da232e
Jack Kolb <jackhunterkolb@gmail.com>
2017-11-20 00:09:14 -0800

    Updated rect_point function, updated controller to plot

--- controller.m
+++ controller.m
@@ -1,7 +1,7 @@
 width = 30;
 length = 30;
-xres = 10;
-yres = 10;
+xres = 30;
+yres = 30;
 
 height = .125;
 load = 30000;
@@ -11,5 +11,6 @@ poiss = .3;
 modu = 30000000;
 
 deform = rect_plate_point_deform(width, length, xres, yres, height, load, x0, y0, poiss, modu);
-[X, Y] = meshgrid(linspace(0, width, xres), linspace(0, length, yres)); 
+[X, Y] = meshgrid(linspace(0, width, xres), linspace(0, length, yres));
+
 surf(X, Y, deform);
--- rect_plate_point_deform.m
+++ rect_plate_point_deform.m
@@ -2,12 +2,11 @@ function [ deformation ] = rect_plate_point_deform( width, length, wres, lres, h
 % calculates the deformation of a rectangular plate (load at point x, y)
 %   returns a matrix of the deformations across the plate surface
 
-    x = linspace(1, width, wres);
-    y = linspace(1, length, lres);
+    x = linspace(0, width, wres);
+    y = linspace(0, length, lres);
+  
+    deformation = zeros([lres wres]);
     
-    deformation = zeros(lres, wres);
-    
-
     D = (modu*height^3) / (12 * (1-poiss^2));
     base_deform = (4*load)/(pi^4*width*length*D);
     
@@ -15,9 +14,9 @@ function [ deformation ] = rect_plate_point_deform( width, length, wres, lres, h
     n = 1:10;
     
     x_count = 0;
-    y_count = 0;
     for xx = x
         x_count = x_count + 1;
+        y_count = 0;
         for yy = y
             y_count = y_count + 1;
             
@@ -31,7 +30,8 @@ function [ deformation ] = rect_plate_point_deform( width, length, wres, lres, h
             deformation(y_count, x_count) = base_deform * sum_deform;
         end    
     end
-
+    
+    disp(deformation);
 %    deformation = (4*load)/(pi^2*width*length*D) * sum( sum( sin(m*pi*x0/width)*sin(n*pi*y0/length) / (m^2/width^2+n^2/length^2) * sin(m*pi*x/width)*sin(n*pi*y/length), n), m); % some random text
 
 end
